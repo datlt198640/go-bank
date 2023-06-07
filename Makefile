bc561d56113fb13b9c176f2f100e05c531ce6aee
dat le <dat.lt198640@gmail.com>
2023-06-07 14:33:39 +0700

    implement go-gin framework

--- Makefile
+++ Makefile
@@ -19,4 +19,7 @@ sqlc:
 test:
 	go test -v -cover ./...
 
-.PHONY: postres createdb drop migrateup migratedown sqlc test
+server:
+	go run main.go
+
+.PHONY: postres createdb drop migrateup migratedown sqlc test server
--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,21 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/datlt198640/go-bank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simplebank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
