e847b76dba94f9e3e0832365507af12a26f125b7
ltdat9 <ltdat9@cmcgloba.vn>
2023-06-13 11:33:36 +0700

    add middleware

--- api/account.go
+++ api/account.go
@@ -2,15 +2,16 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/datlt198640/go-bank/db/sqlc"
+	"github.com/datlt198640/go-bank/token"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -21,8 +22,10 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+
 	arg := db.CreateAccountParams{
-		Owner:    req.Owner,
+		Owner:    authPayload.Username,
 		Currency: req.Currency,
 		Balance:  0,
 	}
@@ -65,6 +68,12 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		return
 	}
 
+	if account.Owner != authPayload.Username {
+		err := errors.New("account does not belong to the authenticatd user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
 	ctx.JSON(http.StatusOK, account)
 }
 
@@ -80,7 +89,9 @@ func (server *Server) listAccount(ctx *gin.Context) {
 		return
 	}
 
+	// authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.ListAccountsParams{
+		// Owner:  authPayload.Username,
 		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,27 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/datlt198640/go-bank/db/sqlc"
+	"github.com/datlt198640/go-bank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+
+	config := util.Config{
+		TokenSymetricJey:    util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 
